b47a52457ca57fcdb2b474dba2964c18f03073e5
Ghasak <ghasak.ibrahim@jp.ey.com>
2021-02-13 00:53:09 +0900

    wranglign with js

---
+++ src/services/example.js
@@ -0,0 +1,17 @@
+
+
+
+let a = 10 ;
+let b = 20 ;
+
+console.log('The value of a = ' + a  + '  The value of b = ' + b)
+
+
+for (i = 1; i <= 10 ; i ++) {
+        console.log('the value of i = '  + i )
+}
+
+
+
+
+
